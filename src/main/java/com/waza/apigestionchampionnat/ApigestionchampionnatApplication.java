6df3647a9a3c4e4e62b04569cb962f956a3ecae7
AlexVigneit <a.vigne@it-students.fr>
2024-06-18 11:38:21 +0200

    first commit

---
+++ src/main/java/com/waza/apigestionchampionnat/ApigestionchampionnatApplication.java
@@ -0,0 +1,13 @@
+package com.waza.apigestionchampionnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigestionchampionnatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigestionchampionnatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waza/apigestionchampionnat/ApigestionchampionnatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waza.apigestionchampionnat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigestionchampionnatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
